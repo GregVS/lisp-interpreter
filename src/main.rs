13aee43249e3cfe7bc707ae310df8ae7048a84b0
Gregory Saldanha <gsaldanha2@gmail.com>
2025-01-08 22:32:32 -0800

    Allow running a script file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use crate::functions::fn_print;
+use crate::functions::fn_eval_multiple;
 use crate::interpreter::eval;
+use crate::tokenizer::{Token};
 
 mod parser;
 mod tokenizer;
@@ -9,11 +11,27 @@ mod functions;
 fn main() {
     let mut stack = interpreter::Stack::new();
 
-    loop {
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        let tokens = tokenizer::tokenize_expr(&input);
+    if std::env::args().len() > 1 {
+        // Run using file
+        let file_path = std::env::args().nth(1).unwrap();
+        let file_content = std::fs::read_to_string(file_path).unwrap();
+        let tokens = tokenizer::tokenize_expr(&file_content);
+        let tokens = vec![]
+            .into_iter()
+            .chain(vec![Token::LParen])
+            .chain(tokens.into_iter())
+            .chain(vec![Token::RParen].into_iter())
+            .collect::<Vec<_>>();
         let parsed = parser::parse(&mut tokens.iter().peekable());
-        fn_print(&eval(&parsed, &mut stack));
+        fn_eval_multiple(&parsed, &mut stack);
+    } else {
+        // Run interactive mode
+        loop {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let tokens = tokenizer::tokenize_expr(&input);
+            let parsed = parser::parse(&mut tokens.iter().peekable());
+            fn_print(&eval(&parsed, &mut stack));
+        }
     }
 }
